c35825a0f8ea794a7bf0414aef71670d4e232bdc
arishem51 <arishem51@gmail.com>
2022-08-15 20:11:58 +0700

    feat: MilesStatistics

--- src/feature/Dashboard/Dashboard.tsx
+++ src/feature/Dashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import BarChartStatistics from "../../components/BarChartStatistics";
 import { BreakFluidCard } from "./components/BreakFluidCard";
 import { EnergyCard } from "./components/EnergyCard";
+import MilesStatistics from "./components/MilesStatistics";
 import { RangeCard } from "./components/RangeCard";
 import { TireWearCard } from "./components/TireWearCard";
 
@@ -22,6 +24,10 @@ const CardWrapper = styled.div`
   gap: var(--size-30);
 `;
 
+const StatisticsWrapper = styled.div`
+  margin-top: var(--size-24);
+`;
+
 const Dashboard = () => {
   return (
     <Wrapper>
@@ -32,6 +38,9 @@ const Dashboard = () => {
           <BreakFluidCard chartValue={9} />
           <TireWearCard chartValue={25} />
         </CardWrapper>
+        <StatisticsWrapper>
+          <MilesStatistics />
+        </StatisticsWrapper>
       </Content>
     </Wrapper>
   );
---
+++ src/feature/Dashboard/components/MilesStatistics.tsx
@@ -0,0 +1,127 @@
+import styled from "styled-components";
+import BarChartStatistics from "../../../components/BarChartStatistics";
+
+const data = [
+  {
+    label: "1 PM",
+    value: 4000,
+    id: "id-1",
+  },
+  {
+    label: "Special",
+    value: 4000,
+    isLine: true,
+    id: "id-1s",
+  },
+  {
+    label: "2 PM",
+    value: 3000,
+    id: "id-2",
+  },
+  {
+    label: "Special",
+    value: 4000,
+    isLine: true,
+    id: "id-2s",
+  },
+  {
+    label: "3 PM",
+    value: 2000,
+    id: "id-3",
+  },
+  {
+    label: "Special",
+    value: 4000,
+    isLine: true,
+    id: "id-3s",
+  },
+  {
+    label: "4 PM",
+    value: 2780,
+    id: "id-4",
+  },
+  {
+    label: "Special",
+    value: 4000,
+
+    isLine: true,
+    id: "id-4s",
+  },
+  {
+    label: "5 PM",
+    value: 1890,
+    id: "id-5",
+  },
+  {
+    label: "Special",
+    value: 4000,
+    isLine: true,
+    id: "id-5s",
+  },
+  {
+    label: "6 PM",
+    value: 2390,
+    id: "id-6",
+  },
+  {
+    label: "Special",
+    value: 4000,
+    isLine: true,
+    id: "id-6s",
+  },
+  {
+    label: "7 PM",
+    value: 3490,
+    id: "id-7",
+  },
+];
+
+const Wrapper = styled.div`
+  padding: var(--size-16) var(--size-32);
+  background-color: var(--color-white);
+  border-radius: var(--size-14);
+  gap: var(--size-10);
+  display: flex;
+  flex-direction: column;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  gap: var(--size-8);
+`;
+
+const Text = styled.p`
+  font-size: var(--size-20);
+  color: var(--color-dark);
+`;
+
+const Text700 = styled(Text)`
+  font-weight: 700;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+`;
+
+const MilesStatistics = () => {
+  return (
+    <Wrapper>
+      <TextWrapper>
+        <Text700>Miles</Text700>
+        <Text>Statistics</Text>
+      </TextWrapper>
+      <ButtonWrapper>
+        <button>Button 1</button>
+        <button>Button 2</button>
+      </ButtonWrapper>
+      <BarChartStatistics
+        dataKey="value"
+        data={data}
+        chartHeight={198}
+        chartWidth={400}
+      />
+    </Wrapper>
+  );
+};
+
+export default MilesStatistics;
