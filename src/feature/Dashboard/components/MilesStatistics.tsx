2a8e314674e6a4bac852204bc318bcc3275f461c
arishem51 <arishem51@gmail.com>
2022-08-15 21:08:17 +0700

    feat: MilesStatistics

--- src/feature/Dashboard/components/MilesStatistics.tsx
+++ src/feature/Dashboard/components/MilesStatistics.tsx
@@ -1,3 +1,4 @@
+import { Fragment, useState } from "react";
 import styled from "styled-components";
 import BarChartStatistics from "../../../components/BarChartStatistics";
 
@@ -101,19 +102,67 @@ const Text700 = styled(Text)`
 
 const ButtonWrapper = styled.div`
   display: flex;
+  gap: var(--size-10);
 `;
 
+const Button = styled.button<{ active: boolean }>`
+  outline: none;
+  appearance: none;
+  border: none;
+  padding: var(--size-4) var(--size-12);
+  border-radius: var(--size-24);
+  background-color: ${(props) =>
+    props.active ? "var(--color-blue)" : "var(--color-white)"};
+  color: ${(props) =>
+    props.active ? "var(--color-white)" : "var(--color-dark2)"};
+  cursor: pointer;
+`;
+
+type ButtonItem = {
+  name: string;
+  isActive: boolean;
+  id: string;
+};
+
+const ButtonArr: ButtonItem[] = [
+  {
+    name: "Day",
+    isActive: false,
+    id: "day",
+  },
+  {
+    name: "Week",
+    isActive: false,
+    id: "week",
+  },
+  {
+    name: "Month",
+    isActive: false,
+    id: "month",
+  },
+];
+
 const MilesStatistics = () => {
+  const [buttonId, setButtonId] = useState<string>(ButtonArr["0"].id);
+
+  const renderButton = () => {
+    return ButtonArr.map((button) => (
+      <Button
+        active={button.id === buttonId}
+        onClick={() => setButtonId(button.id)}
+      >
+        {button.name}
+      </Button>
+    ));
+  };
+
   return (
     <Wrapper>
       <TextWrapper>
         <Text700>Miles</Text700>
         <Text>Statistics</Text>
       </TextWrapper>
-      <ButtonWrapper>
-        <button>Button 1</button>
-        <button>Button 2</button>
-      </ButtonWrapper>
+      <ButtonWrapper>{renderButton()}</ButtonWrapper>
       <BarChartStatistics
         dataKey="value"
         data={data}
